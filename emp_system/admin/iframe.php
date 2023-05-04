be5c50860aa3d3a849c5703f8b33f82a2f6af388
Diksheeta S. Patinge <117720186+pdiksheeta@users.noreply.github.com>
2023-05-04 14:24:28 +0530

    Delete emp_system directory

--- emp_system/admin/iframe.php
+++
@@ -1,6 +0,0 @@
-<?php
-include('admin.php');
-?>
-<iframe src="create_team.php" border-frame="0" align="center" style="border:none; position:absolute; height:79%; width:85%; padding-top:102px;padding-left:205px; margin-top:92px;"></iframe>
-</body>
-</html>
